658aa1e855783d9659c32f502b408b10d7ab2ff5
yabselam <yabsirahoji@gmail.com>
2024-11-17 23:25:58 -0800

    vite.config file update

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/your-repo-name/", // Replace 'your-repo-name' with the name of your repository
   plugins: [react()],
+  base: "/Netflix-Clone/", // Ensure the base matches your repository name
 });
